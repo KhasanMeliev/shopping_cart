c2bfd3fa63a31b127cf0375d400be4e5ba508a9e
khusanmeliev <khusanmeliev@gmail.com>
2022-06-29 08:40:34 +0500

    added changes

--- src/ProductsObj/productObj.js
+++ src/ProductsObj/productObj.js
@@ -1,43 +1,49 @@
+import osh from "../assets/img/osh.jpg";
+import somsa from "../assets/img/somsa.jpg";
+import shashlik from "../assets/img/shashlik.jfif";
+import pizza from "../assets/img/pizza.jpeg";
+import cocaCola from "../assets/img/cocaCola.jpg";
+import pepsi from "../assets/img/pepsi.jpg";
 const initialState = {
   products: [
     {
       id: 1,
-      img: "https://thumbs.dreamstime.com/b/pepsi-drink-cold-soft-caffeine-white-liquid-60265355.jpg",
+      img: osh,
       name: "OSH",
       price: 20000,
       amount: 0,
     },
     {
       id: 2,
-      img: "https://thumbs.dreamstime.com/b/pepsi-drink-cold-soft-caffeine-white-liquid-60265355.jpg",
+      img: somsa,
       name: "SOMSA",
       price: 10000,
       amount: 0,
     },
     {
       id: 3,
-      img: "https://thumbs.dreamstime.com/b/pepsi-drink-cold-soft-caffeine-white-liquid-60265355.jpg",
+      img: shashlik,
       name: "SHASHLIK",
       price: 25000,
       amount: 0,
     },
     {
       id: 4,
-      img: "https://thumbs.dreamstime.com/b/pepsi-drink-cold-soft-caffeine-white-liquid-60265355.jpg",
+      img: pizza,
       name: "PIZZA",
       price: 60000,
       amount: 0,
     },
     {
       id: 5,
-      img: "https://thumbs.dreamstime.com/b/pepsi-drink-cold-soft-caffeine-white-liquid-60265355.jpg",
+      img: cocaCola,
       name: "COCA-COLA",
       price: 20000,
       amount: 0,
     },
     {
       id: 6,
-      img: "https://thumbs.dreamstime.com/b/pepsi-drink-cold-soft-caffeine-white-liquid-60265355.jpg  ",
+      img: pepsi,
       name: "PEPSI",
       price: 15000,
       amount: 0,
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+
 const Login = () => {
+  const values = { name: "", email: "", password: "" };
+  const [details, setDetails] = useState(values);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (details.name === null || details.name === "") {
+      alert("Name isn't blank!");
+      return false;
+    } else if (details.email === null || details.email === "") {
+      alert("Email isn't blank");
+      return false;
+    } else if (details.password === null || details.password === "") {
+      alert("Password isn't blank");
+      return false;
+    } else {
+      alert("your message succesfully sent");
+      return true;
+    }
+  };
+
   return (
     <div className="login">
-      <form action="/contact">
+      <form
+        action="https://formsubmit.co/khasanmeliev2@gmail.com"
+        onSubmit={submitHandler}
+      >
         <h1>Login</h1>
         <div className="inputs">
-          <input type="text" placeholder="Name..." />
-          <input type="email" placeholder="Email..." />
-          <input type="password" placeholder="Password" />
+          <input
+            type="text"
+            placeholder="Name..."
+            value={details.name}
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            name="name"
+            autoComplete="off"
+          />
+          <input
+            type="email"
+            placeholder="Email..."
+            value={details.email}
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            name="email"
+            autoComplete="off"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={details.password}
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            name="password"
+            autoComplete="off"
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
